5d5cd4c37b3d929bed8f1bcaf48024e435b5d7b0
scossar <scossar3@gmail.com>
2015-10-25 09:22:20 -0700

    add rexexp to data tag

--- assets/javascripts/whitelist-classes.js
+++ assets/javascripts/whitelist-classes.js
@@ -1,5 +1,5 @@
 (function () {
   Discourse.Markdown.whiteListTag('span', 'class', 'large-letter');
   Discourse.Markdown.whiteListTag('img', 'class', 'header-image');
-  Discourse.Markdown.whiteListTag('img', 'data-max-height', '*' );
+  Discourse.Markdown.whiteListTag('img', 'data-max-height', /\d+/ );
 })();
