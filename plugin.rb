994ad8653c0743db52ae689cb864dec7dae2322a
scossar <scossar3@gmail.com>
2016-01-04 20:45:50 -0800

    add url

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,7 @@
 # about: Creates a blog-post category for Discourse
 # version: 0.1
 # authors: scossar
+# url: https://github.com/scossar/discourse-blog-post.git
 
 enabled_site_setting :blog_post_enabled
 register_asset 'javascripts/whitelist-classes.js', :server_side
